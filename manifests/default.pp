187e315da8e9e4c43bea0a949a6a42646aed0c1a
tomasz <tomasz@noemail.com>
2013-05-22 15:16:39 +0300

    jenkis running

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,52 +1,62 @@
-exec { 'aptupdate' :
-    path => "/usr/bin",
-    command => 'apt-get update',
-    user => 'root'
+include apt
+include jenkins
+
+apt::ppa { "ppa:webupd8team/java": 
+}
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+  require => Apt::Ppa["ppa:webupd8team/java"]
+}
+
+jenkins::plugin { 'git': 
+
 }
 
 
-$reqPackages = [ "subversion", "vim-gtk", "tar", "wget", "git-core", "git-gui" ]
-package { $reqPackages: ensure => "installed", require => Exec["aptupdate"] }
-exec { "jvmdir":
-    path => '/bin:/usr/bin',
-    creates => "/usr/lib/jvm",
-    command => 'mkdir -p /usr/lib/jvm',
-    user => "root"
+jenkins::plugin { 'jenkow-plugin': 
 }
 
-exec { "jdk7" :
-    path => '/bin:/usr/bin',
-    require => [Package["tar"], Exec["jvmdir"]],
-    command => 'tar -xzf /vagrant/jdk-7u10-linux-i586.tar.gz -C /usr/lib/jvm/',
-    creates => "/usr/lib/jvm/jdk",
-    user => "root"
+jenkins::plugin { 'async-job': 
 }
 
-exec { "mvn" :
-    path => '/bin:/usr/bin',
-    require => [Package["tar"], Package["wget"]],
-    command => 'wget -O - "http://apache.mirror.vu.lt/apache/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz" | tar -xvzf - -C /home/vagrant',
-    creates => "/home/vagrant/apache-maven-3.0.4"
+jenkins::plugin { 'git-server': 
 }
 
-file { "/etc/profile.d/java_home.sh" :
-    ensure => 'present',
-    source => '/vagrant/scripts/java_home.sh'
+jenkins::plugin { 'database': 
+
 }
 
-file { "/etc/profile.d/maven_home.sh" :
-    ensure => 'present',
-    source => '/vagrant/scripts/maven_home.sh'
+jenkins::plugin { 'git-client': 
+
 }
 
-$jdk_version = '7u3'
-$jdk_version_long = "${jdk_version}-b04"
-$jdk_rpm_filename = "jdk-${jdk_version}-linux-x64.rpm"
-$jdk_rpm_location = "/vagrant-share/apps/${rpm_filename}"
+Jenkins::Plugin { notify => Class[jenkins::service] }
 
-exec { 'oraclejdk7':
-     cwd => '/vagrant/scripts',
-     command => "wget http://download.oracle.com/otn-pub/java/jdk/${jdk_version_long}/jdk-${jdk_version}-linux-x64.rpm",
-     path => '/bin:/usr/bin'
+
+exec {
+  "accept_license":
+  command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+  cwd => "/home/vagrant",
+  user => "vagrant",
+  path => "/usr/bin/:/bin/",
+  before => Package["oracle-java7-installer"],
+  logoutput => true,
 }
+
+$reqPackages = [ "git-core", "git-gui"]
+package { 
+    $reqPackages: ensure => "installed"
+}
+
+package { "oracle-java7-installer":
+    ensure => "installed",
+    require => Exec["apt-get update"]
+}
+
+package { "maven":
+    ensure => "installed",
+    before => Package["oracle-java7-installer"]
+}
+
 
