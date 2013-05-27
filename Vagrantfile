4d00824a688d85a5f5824aa0777293c91da6be94
tomasz <tomasz@noemail.com>
2013-05-27 10:28:38 +0300

    postgresql service

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,10 @@
 Vagrant.configure("2") do |config|
-    config.vm.box = "precise32"    
-    config.vm.box_url = "../precise32.box"
-    
+    config.vm.box = "cen"  
+    config.vm.box_url = "../centos64.box"
+#    config.vm.box_url = "https://dl.dropbox.com/u/7196/vagrant/CentOS-56-x64-packages-puppet-2.6.10-chef-0.10.6.box"
     config.ssh.forward_x11 = :true
 
-    config.vm.network :forwarded_port, guest: 8080, host: 8080
+    config.vm.network :forwarded_port, guest: 5432, host: 5432
     config.vm.provision :shell, :path => "shell/main.sh"
     config.vm.provision :puppet
 
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,52 +1,33 @@
-exec { 'aptupdate' :
-    path => "/usr/bin",
-    command => 'apt-get update',
-    user => 'root'
+ /*  include postgresql::server */
+class { 'postgresql::server':
+  config_hash => {
+    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+    'ip_mask_allow_all_users'    => '0.0.0.0/0',
+    'listen_addresses'           => '*',
+    'ipv4acls'                   => ['hostssl all johndoe 192.168.0.0/24 cert'],
+    'manage_redhat_firewall'     => true,
+    'postgres_password'          => 'TPSrep0rt!',
+  },
+}
+
+/*class { 'postgresql':
+
+}*/
+/*-> class { 'postgresql::server':
+}*/
+
+
+/*
+$reqPackages = [ "postgresql84-server" ]
+
+package { 
+    $reqPackages: ensure => "installed"
+} */
+
+postgresql::db { 'junit':
+  user     => 'aa',
+  password => 'aa'
 }
 
 
-$reqPackages = [ "subversion", "vim-gtk", "tar", "wget", "git-core", "git-gui" ]
-package { $reqPackages: ensure => "installed", require => Exec["aptupdate"] }
-exec { "jvmdir":
-    path => '/bin:/usr/bin',
-    creates => "/usr/lib/jvm",
-    command => 'mkdir -p /usr/lib/jvm',
-    user => "root"
-}
-
-exec { "jdk7" :
-    path => '/bin:/usr/bin',
-    require => [Package["tar"], Exec["jvmdir"]],
-    command => 'tar -xzf /vagrant/jdk-7u10-linux-i586.tar.gz -C /usr/lib/jvm/',
-    creates => "/usr/lib/jvm/jdk",
-    user => "root"
-}
-
-exec { "mvn" :
-    path => '/bin:/usr/bin',
-    require => [Package["tar"], Package["wget"]],
-    command => 'wget -O - "http://apache.mirror.vu.lt/apache/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz" | tar -xvzf - -C /home/vagrant',
-    creates => "/home/vagrant/apache-maven-3.0.4"
-}
-
-file { "/etc/profile.d/java_home.sh" :
-    ensure => 'present',
-    source => '/vagrant/scripts/java_home.sh'
-}
-
-file { "/etc/profile.d/maven_home.sh" :
-    ensure => 'present',
-    source => '/vagrant/scripts/maven_home.sh'
-}
-
-$jdk_version = '7u3'
-$jdk_version_long = "${jdk_version}-b04"
-$jdk_rpm_filename = "jdk-${jdk_version}-linux-x64.rpm"
-$jdk_rpm_location = "/vagrant-share/apps/${rpm_filename}"
-
-exec { 'oraclejdk7':
-     cwd => '/vagrant/scripts',
-     command => "wget http://download.oracle.com/otn-pub/java/jdk/${jdk_version_long}/jdk-${jdk_version}-linux-x64.rpm",
-     path => '/bin:/usr/bin'
-}
 
