2d589fff2f10163bf582d31aca45a7577fc74c74
tomas <timoras@mailinator.com>
2013-07-30 17:45:43 +0300

    next gerrit

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,6 @@ Vagrant.configure("2") do |config|
     config.ssh.forward_x11 = :true
 
     config.vm.network :forwarded_port, guest: 8080, host: 8080
-    config.vm.provision :shell, :path => "shell/main.sh"
+    config.vm.network :forwarded_port, guest: 8180, host: 8180
     config.vm.provision :puppet
-
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,32 @@
-Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+package { 'git':
+    ensure => 'installed'   
+}
 
+exec {'apt-get update':
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]    
+}->
+package { 'openjdk-7-jdk':
+    ensure => 'installed'   
+}->
+file { "/opt/gerrit": 
+   path    => "/opt/gerrit", 
+   mode    => 0777
+}->
 package { 'wget':
     ensure => 'installed'   
-}~>
+}->
 exec { 'download_gerrit':
     command => "wget -q http://gerrit-releases.storage.googleapis.com/gerrit-2.6.1.war",
-    creates => "/home/vagrant/gerrit-2.6.1.war"
-}~>
+    creates => "/home/vagrant/gerrit-2.6.1.war",
+    cwd => "/home/vagrant/",
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]    
+}->
 exec {'gerrit_init':
-    command => "java -jar /home/vagrant/gerrit-2.6.1.war init"
+    command => "java -jar /home/vagrant/gerrit-2.6.1.war init -D/opt/gerrit",
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]    
+}->
+file {'/opt/gerrit':
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]    
 }
 
 
